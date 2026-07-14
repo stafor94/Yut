dd8169be4fa583abdfb4dbd1239731e87da39357
stafor94 <stafor@naver.com>
2026-07-14 11:27:05 +0900

    앱 전역 커스텀 알림 호스트 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import { CustomAlertHost } from './app/components/CustomAlertHost';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    <CustomAlertHost />
+  </React.StrictMode>,
+);
