0e50ca74010740a1a5eeb977269fe0b134020efb
stafor94 <stafor@naver.com>
2026-07-16 07:08:28 +0900

    인게임 정렬 오버라이드 로드

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import { CustomAlertHost } from './app/components/CustomAlertHost';
 import './styles/mobile-item-log-recovery.css';
+import './styles/game-header-log-alignment.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
