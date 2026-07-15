035b78b361e63376a4002fd108284dfdb0c6bc44
stafor94 <stafor@naver.com>
2026-07-16 06:30:26 +0900

    아이템전 모바일 진행 기록 복구 스타일 연결

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import { CustomAlertHost } from './app/components/CustomAlertHost';
+import './styles/mobile-item-log-recovery.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
