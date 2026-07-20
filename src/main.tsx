d2eb5f93f195380a2d4d0ceee1ed282933bdefff
stafor94 <stafor@naver.com>
2026-07-20 22:57:34 +0900

    방 목록 조회 상태 스타일 연결

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ import './styles/lobby-portrait-fit.css';
 import './styles/lobby-requested-polish.css';
 import './styles/lobby-guide-polish.css';
 import './styles/lobby-dialog-position.css';
+import './styles/lobby-room-query-loading.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
