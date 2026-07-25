76a6c5144795d5ad351f3415fcb77a66b0d0ca70
stafor94 <stafor@naver.com>
2026-07-25 22:38:10 +0900

    WebKit lane에 Firebase browser isolation 연결

--- tests/qa/project-contracts.mjs
+++ tests/qa/project-contracts.mjs
@@ -7,7 +7,10 @@ export const qaProjectTestMatches = Object.freeze({
     /regression\/.*\.spec\.js$/u,
   ]),
   'mobile-galaxy': Object.freeze([/mobile\/.*\.spec\.js$/u]),
-  'mobile-webkit-timing': Object.freeze([/mobile\/roll-timing-pointer-capture\.spec\.js$/u]),
+  'mobile-webkit-timing': Object.freeze([
+    /smoke\/firebase-emulator-isolation\.spec\.js$/u,
+    /mobile\/roll-timing-pointer-capture\.spec\.js$/u,
+  ]),
 });
 
 export const qaProjectNames = Object.freeze(Object.keys(qaProjectTestMatches));
